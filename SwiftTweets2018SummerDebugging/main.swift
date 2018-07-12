ca34e3a7cef3b324ab8176ade9004198c5aacc65
Kuniwak <orga.chem.job@gmail.com>
2018-07-12 19:31:26 +0900

    tmp

--- SwiftTweets2018SummerDebugging/main.swift
+++ SwiftTweets2018SummerDebugging/main.swift
@@ -1,12 +1,18 @@
-//
-//  main.swift
-//  SwiftTweets2018SummerDebugging
-//
-//  Created by Yuki Kokubun on 2018/07/12.
-//  Copyright © 2018年 Yuki Kokubun. All rights reserved.
-//
+func hoge() {
+    let message = "hoge"
+    print(message)
+    fuga()
+}
 
-import Foundation
+func fuga() {
+    let message = "fuga"
+    print(message)
+    moge()
+}
 
-print("Hello, World!")
+func moge() {
+    let message = "moge"
+    print(message)
+}
 
+hoge()
